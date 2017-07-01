9128fc7981f818272b8b22882a93b1ddf66c94d8
amasson42 <gwarthur@laposte.net>
2017-07-01 20:11:26 +0200

    little documentation

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,8 +1,12 @@
 
 import Cocoa
 
+// Creation of the shared instance of application
 NSApplication.shared()
 NSApp.setActivationPolicy(.regular)
+let appName = ProcessInfo.processInfo.processName
+
+// Creation of a menu bar and his items
 
 let menubar = NSMenu()
 let appMenuItem = NSMenuItem()
@@ -10,7 +14,6 @@ menubar.addItem(appMenuItem)
 NSApp.mainMenu = menubar
 
 let appMenu = NSMenu()
-let appName = ProcessInfo.processInfo.processName
 let quitTitle = "Quit " + appName
 let quitMenuItem = NSMenuItem(title: quitTitle,
                               action: #selector(NSApp.terminate),
@@ -18,6 +21,7 @@ let quitMenuItem = NSMenuItem(title: quitTitle,
 appMenu.addItem(quitMenuItem)
 appMenuItem.submenu = appMenu
 
+// Creation of the main window
 let windowRect = NSMakeRect(0, 0, 600, 400)
 let window = NSWindow(contentRect: windowRect,
                       styleMask: [.miniaturizable, .titled, .closable],
@@ -27,8 +31,10 @@ window.cascadeTopLeft(from: NSPoint(x: 200, y: 200))
 window.title = appName
 window.makeKeyAndOrderFront(nil)
 
+// Creation of the personalised view and association to the main window
 let view = MyView(frame: windowRect, options: nil)
 window.contentView?.addSubview(view)
 
+// Start the looping event to run the application
 NSApp.activate(ignoringOtherApps: true)
 NSApp.run()
